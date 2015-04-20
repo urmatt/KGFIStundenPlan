180e77e8459027063ddc5d7ea20e1635f50418b4
Urma <urma.93.kg@gmail.com>
2015-04-20 19:57:43 +0600

    Кнопки загружаются из базы данных. Из таблицы groups.

--- app/src/main/java/student/kgfi/kgfistundenplan/MainActivity.java
+++ app/src/main/java/student/kgfi/kgfistundenplan/MainActivity.java
@@ -1,31 +1,49 @@
 package student.kgfi.kgfistundenplan;
 
+import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 import android.widget.GridLayout;
 import android.widget.Button;
 import android.graphics.drawable.Drawable;
 
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
 
-    String [] groups = new String []{"ИГ-1-11", "ИГ-1-12", "ИГ-1-13", "ИГ-2-13", "ИГ-1-14"};
 
     GridLayout gLayout;
+    ArrayList<String> list = new ArrayList<>();
+    HashMap<Integer, Button> buttonsMap = new HashMap<Integer, Button>();
+    DisplayMetrics displayMetrics = null;
+    WebView mainWeb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gLayout = (GridLayout) findViewById(R.id.mainGrid);
-        for(String g: groups){
-            Button b = new Button(this);
-            b.setText(g);
-            b.setSingleLine();
-//            b.setBackground(getDrawable(R.drawable.main_button));
-            b.setTextColor(255);
-        }
+        mainWeb = (WebView) findViewById(R.id.mainWeb);
+        mainWeb.loadUrl("http://kgfi.ksucta.kg/index.php/ru/news");
+        displayMetrics = getResources().getDisplayMetrics();
+        new LoadCategories().execute();
+
     }
 
 
@@ -49,5 +67,75 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class LoadCategories extends AsyncTask<String, String, String> {
+
+        ProgressDialog pDialog;
+        @Override
+        protected void onPreExecute() {
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            try {
+                // getting JSON string from URL
+                JSONObject jsonObject = JSONRequest.httpRequest("http://urmapps.esy.es/mykgfi/getGroups.php", JSONRequest.Method.POST, params);
+                JSONArray jsonArray = jsonObject.getJSONArray("groups");
+                Log.d("", jsonArray.toString());
+                //
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject oneObject = jsonArray.getJSONObject(i);
+                    String name = oneObject.getString("g_name");
+//                    Log.d("Categories Name ---", name);
+                    list.add(name);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Creating & adding buttons
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            int widthForB;
+            int displayW = displayMetrics.widthPixels;
+            widthForB = displayW/3 - 30;
+            int ids = new Integer(getTaskId());
+
+
+
+            for(int i = 0; i < list.size(); i++){
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams(gLayout.getLayoutParams());
+                params.setMargins(12,12,12,12);
+                Button b = new Button(MainActivity.this);
+                b.setText(list.get(i));
+                b.setClickable(true);
+                b.setSelected(true);
+                b.setSingleLine();
+                b.setTextSize(20);
+                b.setLayoutParams(params);
+                b.setId(ids);
+                b.setTextColor(Color.parseColor("#ffffff"));
+                b.setBackgroundResource(R.drawable.main_button);
+                b.setWidth(widthForB);
+                b.setHeight(widthForB);
+                gLayout.addView(b);
+                buttonsMap.put(ids, b);
+                ids++;
+            }
+
+            pDialog.dismiss();
+            //////-----------------
+            super.onPostExecute(s);
+        }
     }
 }
